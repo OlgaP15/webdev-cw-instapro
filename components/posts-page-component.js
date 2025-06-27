f758d3a74a0fa49b5d2b04fa83f57bb98b2dccc0
OlgaP <zjkz14@gmail.com>
2025-06-27 21:06:03 +0300

    проверка

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -44,7 +44,6 @@ export function renderPostsPageComponent({ appEl }) {
                         <p class="post-header__user-name">Варварва Н.</p>
                     </div>
                   
-                    
                     <div class="post-image-container">
                       <img class="post-image" src="https://storage.yandexcloud.net/skypro-webdev-homework-bucket/1680670675451-%25C3%2590%25C2%25A1%25C3%2590%25C2%25BD%25C3%2590%25C2%25B8%25C3%2590%25C2%25BC%25C3%2590%25C2%25BE%25C3%2590%25C2%25BA%2520%25C3%2591%25C2%258D%25C3%2590%25C2%25BA%25C3%2591%25C2%2580%25C3%2590%25C2%25B0%25C3%2590%25C2%25BD%25C3%2590%25C2%25B0%25202023-03-31%2520%25C3%2590%25C2%25B2%252012.51.20.png">
                     </div>
